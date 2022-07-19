4824e0ff460c487e804beb469939b2bfbefb6c99
hoon <dohun10170@gmail.com>
2022-07-19 10:54:13 +0900

    querydsl 설정

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,10 +1,19 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
 class QuerydslApplicationTests {
+
 
 	@Test
 	void contextLoads() {
